d33ef0436cc0e8aa62c5be545ec7018b19ff3cd6
jeongsik <ijaeo@Jeongsiks-Macmini.local>
2023-10-24 00:41:07 +0900

    Auto commit process_checker at 2023-10-24 00:41:07

--- process_checker.sh
+++ process_checker.sh
@@ -4,14 +4,14 @@ source ~/opt/anaconda3/etc/profile.d/conda.sh
 
 cd ~/Development/24H1_QE_Coding
 
-echo "Pulling from git..."
+echo "\nPulling from git..."
 git pull
 
-echo "Running process_checker.py..."
+echo "\nRunning process_checker.py..."
 python ./process_checker.py
 
 
-echo "Pushing to git..."
+echo "\nPushing to git..."
 timestamp=$(date +"%Y-%m-%d %T")
 git add .
 git commit -m "Auto commit process_checker at $timestamp"
